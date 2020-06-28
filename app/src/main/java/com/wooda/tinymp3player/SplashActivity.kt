ebccd8054b759b4ec4a5a23d58a919de84e7d464
wooda <wooda@woodaui-iMac.local>
2020-06-28 17:07:56 +0900

    Kotlin coroutine is applied to async loading

--- app/src/main/java/com/wooda/tinymp3player/SplashActivity.kt
+++ app/src/main/java/com/wooda/tinymp3player/SplashActivity.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var mStatusText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
-
         setContentView(R.layout.activity_splash)
+
+        mStatusText = findViewById(R.id.statusMessage)
+
+        GlobalScope.launch(Dispatchers.Main)
+        {
+            val fileCount = buildAllMp3FilesAsync().await()
+            mStatusText.text = "$fileCount files are loaded."
+        }
     }
+
+    private fun buildAllMp3FilesAsync() : Deferred<Int> =
+        GlobalScope.async(Dispatchers.IO) {
+            Thread.sleep(10_000)
+            return@async 10
+        }
 }
