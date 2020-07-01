26df0b87c899fb71b1b0bac3da49887109ed1403
wooda <wooda77@gmail.com>
2020-07-01 23:06:36 +0900

    SplashActivity is refactoried using MVP pattern

--- app/src/main/java/com/wooda/tinymp3player/SplashActivity.kt
+++ app/src/main/java/com/wooda/tinymp3player/SplashActivity.kt
@@ -10,17 +10,18 @@ import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.wooda.tinymp3player.presenter.SplashPresenter
 import kotlinx.coroutines.*
 import java.lang.Exception
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : SplashPresenter.View, AppCompatActivity() {
 
     companion object {
         private const val TAG = "SplashActivity"
-        private const val READ_STORAGE_PERMISSION_REQUEST_CODE = 101
     }
 
     private lateinit var mStatusText: TextView
+    private lateinit var mPresenter: SplashPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,48 +30,8 @@ class SplashActivity : AppCompatActivity() {
 
         mStatusText = findViewById(R.id.statusMessage)
 
-        if (!checkPermissionForReadExternalStorage()) {
-            Log.i(TAG, "Requesting READ_EXTERNAL_STORAGE permission.")
-            requestPermissionForReadExternalStorage()
-        } else {
-            startInitialize()
-        }
-    }
-
-    private fun startInitialize() {
-        GlobalScope.launch(Dispatchers.Main)
-        {
-            val fileCount = buildAllMp3FilesAsync().await()
-            mStatusText.text = "$fileCount files are loaded."
-        }
-    }
-
-    private fun buildAllMp3FilesAsync() : Deferred<Int> =
-        GlobalScope.async(Dispatchers.IO) {
-            val mediaFiles = getAllAudioFromDevice(applicationContext)
-            return@async mediaFiles.size
-        }
-
-    // referenced from: https://stackoverflow.com/questions/43148879/how-to-get-read-external-storage-permissions
-    private fun checkPermissionForReadExternalStorage(): Boolean {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val result = applicationContext.checkSelfPermission((Manifest.permission.READ_EXTERNAL_STORAGE))
-            return result == PackageManager.PERMISSION_GRANTED
-        }
-        return false;
-    }
-
-    private fun requestPermissionForReadExternalStorage() {
-        try {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                READ_STORAGE_PERMISSION_REQUEST_CODE
-            )
-        } catch (e: Exception) {
-            e.printStackTrace()
-            throw e
-        }
+        mPresenter = SplashPresenter(this, this)
+        mPresenter.initializePlayList()
     }
 
     override fun onRequestPermissionsResult(
@@ -78,54 +39,15 @@ class SplashActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when (requestCode) {
-            READ_STORAGE_PERMISSION_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startInitialize()
-                } else {
-                    // Requesting permission is denied.
-                    Log.i(TAG, "Requesting permission is denied. Finish application")
-                    finish()
-                }
-            }
-        }
+        mPresenter.onPermissionResultReceived(requestCode, permissions, grantResults)
     }
 
-    // referenced from: https://stackoverflow.com/questions/39461954/list-all-mp3-files-in-android
-    private fun getAllAudioFromDevice(context: Context) : List<AudioModel> {
-        val result = mutableListOf<AudioModel>()
-
-        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        val projection = arrayOf<String>(
-            MediaStore.Audio.AudioColumns.DATA, // todo: check deprecated
-            MediaStore.Audio.AudioColumns.ALBUM,
-            MediaStore.Audio.ArtistColumns.ARTIST
-        )
-        val c = context.contentResolver.query(
-            uri,
-            projection,
-            null,
-            null,
-            null)
-
-        if (c != null) {
-            while (c.moveToNext()) {
-                val path = c.getString(0)
-                val album = c.getString(1)
-                val artist = c.getString(2)
-                val name = path.substring(path.lastIndexOf("/") + 1)
-
-                val audioModel = AudioModel(
-                    path,
-                    name,
-                    album,
-                    artist
-                )
-
-                Log.d(TAG, "Media file is found: $audioModel")
-                result.add(audioModel)
-            }
-        }
-        return result
+    override fun updateStatus(statusMessage: String) {
+        mStatusText.text = statusMessage
     }
+
+    override fun requestFinish() {
+        finish()
+    }
+
 }
---
+++ app/src/main/java/com/wooda/tinymp3player/presenter/SplashPresenter.kt
@@ -0,0 +1,127 @@
+package com.wooda.tinymp3player.presenter
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.wooda.tinymp3player.AudioModel
+import com.wooda.tinymp3player.SplashActivity
+import kotlinx.coroutines.*
+import java.lang.Exception
+
+class SplashPresenter(val mActivity: Activity, val mSplashView: View) {
+
+    fun initializePlayList() {
+
+        // check permission
+        if (!checkPermissionAcquired()) {
+            Log.i(TAG, "Requesting READ_EXTERNAL_STORAGE permission.")
+            requestPermission()
+        } else {
+            initializePlayListWithoutPermissionCheck()
+        }
+    }
+
+    private fun initializePlayListWithoutPermissionCheck() {
+        GlobalScope.launch(Dispatchers.Main)
+        {
+            Log.i(TAG, "Starting initialize playlist.")
+            val fileCount = buildAllMp3FilesAsync().await()
+            mSplashView.updateStatus("$fileCount files are loaded.")
+        }
+    }
+
+    private fun buildAllMp3FilesAsync() : Deferred<Int> =
+        GlobalScope.async(Dispatchers.IO) {
+            val mediaFiles = getAllAudioFromDevice(mActivity)
+            return@async mediaFiles.size
+        }
+
+    // referenced from: https://stackoverflow.com/questions/39461954/list-all-mp3-files-in-android
+    private fun getAllAudioFromDevice(context: Context) : List<AudioModel> {
+        val result = mutableListOf<AudioModel>()
+
+        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        val projection = arrayOf<String>(
+            MediaStore.Audio.AudioColumns.DATA, // todo: check deprecated
+            MediaStore.Audio.AudioColumns.ALBUM,
+            MediaStore.Audio.ArtistColumns.ARTIST
+        )
+        val c = context.contentResolver.query(
+            uri,
+            projection,
+            null,
+            null,
+            null)
+
+        if (c != null) {
+            while (c.moveToNext()) {
+                val path = c.getString(0)
+                val album = c.getString(1)
+                val artist = c.getString(2)
+                val name = path.substring(path.lastIndexOf("/") + 1)
+
+                val audioModel = AudioModel(
+                    path,
+                    name,
+                    album,
+                    artist
+                )
+
+                Log.d(TAG, "Media file is found: $audioModel")
+                result.add(audioModel)
+            }
+        }
+        return result
+    }
+
+    fun onPermissionResultReceived(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            READ_STORAGE_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initializePlayListWithoutPermissionCheck()
+                } else {
+                    // Requesting permission is denied.
+                    Log.i(TAG, "Requesting permission is denied. Finish application")
+                    mSplashView.requestFinish()
+                }
+            }
+        }
+    }
+
+    // referenced from: https://stackoverflow.com/questions/43148879/how-to-get-read-external-storage-permissions
+    private fun checkPermissionAcquired(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val result = mActivity.checkSelfPermission((Manifest.permission.READ_EXTERNAL_STORAGE))
+            return result == PackageManager.PERMISSION_GRANTED
+        }
+        return false;
+    }
+
+    private fun requestPermission() {
+        try {
+            ActivityCompat.requestPermissions(
+                mActivity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_STORAGE_PERMISSION_REQUEST_CODE
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw e
+        }
+    }
+
+    companion object {
+        private const val TAG = "SplashPresenter"
+        private const val READ_STORAGE_PERMISSION_REQUEST_CODE = 101
+    }
+
+    interface View {
+        fun updateStatus(statusMessage: String);
+        fun requestFinish()
+    }
+}
